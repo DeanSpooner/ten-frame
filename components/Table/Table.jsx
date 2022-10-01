269149cf850806edd3e522ded3e9565bb58bb419
Dean Spooner <dean.spooner@deptagency.com>
2022-10-01 18:12:52 +0100

    Renamed array in Table.jsx.

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -3,10 +3,10 @@ import tableStyles from "./Table.styles";
 
 export default Table = ({ counter }) => {
   const lengthOfFrame = 10;
-  const myArray = Array.from(Array(lengthOfFrame)).map((e, i) => i + 1);
+  const arrayOfCounters = Array.from(Array(lengthOfFrame)).map((e, i) => i + 1);
   return (
     <View style={tableStyles.container}>
-      {myArray.map((num) => (
+      {arrayOfCounters.map((num) => (
         <View style={tableStyles.cell} key={num}>
           <View style={counter < num ? "" : tableStyles.counter}></View>
         </View>
